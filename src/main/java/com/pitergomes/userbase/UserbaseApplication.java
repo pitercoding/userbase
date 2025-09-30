a692e1d7590ce097cf7166f9d66fedaa6d39a596
pitercoding <piterg.bio@gmail.com>
2025-09-30 13:18:02 +0200

    feat: initial project setup

---
+++ src/main/java/com/pitergomes/userbase/UserbaseApplication.java
@@ -0,0 +1,13 @@
+package com.pitergomes.userbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserbaseApplication.class, args);
+	}
+
+}
